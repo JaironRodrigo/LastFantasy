693985f762c42a2082095e5abede12e9535b9fb3
JaironRodrigo <jaironcreed@gmail.com>
2023-01-11 09:46:31 -0300

    Finalizando a animação do pulo e inserindo a menssagem de morte quando atinge a pedra

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const hero = document.querySelector(".hero");
-const rock = document.querySelector(".rock");
+let hero = document.getElementById("hero");
+let rock = document.getElementById("rock");
 const jump = () => {
   hero.classList.add("jump");
 
@@ -8,13 +8,21 @@ const jump = () => {
   }, 500);
 };
 
-const loop = setInterval(() => {
-  const rockPosition = rock.offsetleft;
-  const heroPosition = +window.getComputedStyle(hero).bottom.replace("px", "");
+const loop = setInterval(function () {
+  var rockPosition = rock.offsetLeft;
+  var heroPosition = +window.getComputedStyle(hero).bottom.replace("px", "");
 
-  if (rockPosition <= 120 && heroPosition < 140) {
+  if (rockPosition < 120 && rockPosition > 0 && heroPosition < 130) {
     rock.style.animation = "none";
+    rock.style.left = `${rockPosition}px`;
+
+    hero.style.animation = "none";
+    hero.style.bottom = `${heroPosition}px`;
+
+    hero.src = "./images/youdied.webp";
+    hero.style.width = "400px";
+    hero.style.marginBottom = "300px";
+    hero.style.marginLeft = "570px";
   }
 }, 10);
-
 document.addEventListener("keydown", jump);
