09112e9148b82766b6dd4a04702dd0689a259824
JaironRodrigo <jaironcreed@gmail.com>
2023-01-13 22:23:49 -0300

    Adicionei o Contador de Pontos, que para ao morrer

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 let hero = document.getElementById("hero");
 let rock = document.getElementById("rock");
+var count = 0;
+
+function score() {
+  count++;
+  var counter = document.getElementById("score");
+  counter.innerHTML = "Score: " + count;
+}
+
+function startCounter() {
+  score();
+  setTimeout(startCounter, 50, window);
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  startCounter();
+});
 const jump = () => {
   hero.classList.add("jump");
 
@@ -11,7 +27,6 @@ const jump = () => {
 const loop = setInterval(function () {
   var rockPosition = rock.offsetLeft;
   var heroPosition = +window.getComputedStyle(hero).bottom.replace("px", "");
-
   if (rockPosition < 100 && rockPosition > 0 && heroPosition < 90) {
     rock.style.animation = "none";
     rock.style.left = `${rockPosition}px`;
@@ -19,10 +34,10 @@ const loop = setInterval(function () {
     hero.style.animation = "none";
     hero.style.bottom = `${heroPosition}px`;
 
-    hero.src = "./images/youdied.webp";
-    hero.style.width = "250px";
-    hero.style.marginBottom = "200px";
-    hero.style.marginLeft = "350px";
+    hero.src = "./images/pixel-skeleton (1).gif";
+    hero.style.width = "100px";
+
+    score = 0;
 
     clearInterval(loop);
   }
